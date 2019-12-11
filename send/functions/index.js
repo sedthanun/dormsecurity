4afed7c939ea9a47b11f3ce42bafaf3c450a936f
sedthanun <62070212@kmitl.ac.th>
2019-12-12 01:55:45 +0700

    Website Commit 28
    google sheet functions/ button link

---
+++ send/functions/index.js
@@ -0,0 +1,56 @@
+const functions = require("firebase-functions");
+
+// Firebase Admin initialization
+var admin = require("firebase-admin");
+var serviceAccount = require("./service-account.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dormsecurity.firebaseio.com/"// https://console.firebase.google.com/u/0/project/{xxxxx}/overview
+});
+
+// Get Google Sheets instance
+const { google } = require("googleapis");
+const sheets = google.sheets("v4");
+
+// Create JWT
+const jwtClient = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"] // read and write sheets
+});
+
+// Get data from RTDB
+exports.copyToSheet = functions.database.ref("/studentbook/{documentId}").onCreate((snapshot, contex) => {
+  let data =  snapshot._data;
+
+  // Convert JSON to Array following structure below
+  /* 
+  [
+    ['COL-A', 'COL-B', 'COL-C', 'COL-D', 'COL-E']
+  ]
+  */
+  var valueArray = [[data.email, data.name, data.room, data.stuid, data.time]]; 
+  var countArray = []
+  countArray.push(valueArray)
+
+  
+  let maxRange = countArray.length + 1;
+
+  // Do authorization
+  jwtClient.authorize();
+  
+  // Create Google Sheets request
+  let request = {
+    auth: jwtClient,
+    spreadsheetId: "1u9g5P4Q8sCsAAANk2cIzLP75rrESv5QqpVeHA1-RWRU",//https://docs.google.com/spreadsheets/d/{yyyyy}/
+    range: "History!A2:E" + maxRange,
+    valueInputOption: "RAW",
+    requestBody: {
+      values: valueArray
+    }
+  };
+  
+  // Update data to Google Sheets
+  sheets.spreadsheets.values.append(request, {});
+});
+
